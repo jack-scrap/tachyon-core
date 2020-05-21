e5319ec8e87923a144878075096535432e4eac01
jackhasakeyboard <jackhasakeyboard@gmail.com>
2020-05-20 19:21:15 -0600

    Format

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	gl.enableVertexAttribArray(attrPos);
 
 	// draw
-	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	gl.clearColor(0, 0, 0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
 	gl.useProgram(prog);
