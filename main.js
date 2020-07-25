f61a9a797da12c99236cbb942b0cc0c9f26df4ae
jackhasakeyboard <jackhasakeyboard@gmail.com>
2020-07-25 08:46:35 -0600

    Squash

--- main.js
+++ main.js
@@ -1,3 +1,53 @@
+class Mesh {
+	constructor(vtc) {
+		// data
+		this.vtc = vtc;
+
+		this.vbo = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vtc), gl.STATIC_DRAW);
+
+		this.prog = new Prog("shad");
+
+		// matrix
+		this.model = new Float32Array(16);
+
+		mat4.identity(this.model);
+
+		this.id = new Float32Array(16);
+		this.trans = new Float32Array(16);
+		this.rot = new Float32Array(16);
+
+		mat4.identity(this.id);
+
+		// attribute
+		this.attrPos = gl.getAttribLocation(this.prog.id, 'pos');
+		gl.vertexAttribPointer(this.attrPos, 2, gl.FLOAT, gl.FALSE, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
+		gl.enableVertexAttribArray(this.attrPos);
+
+		// uniform
+		this.uniModel = gl.getUniformLocation(this.prog.id, 'model');
+
+		this.y = 0;
+		this.ang = 0;
+	}
+
+	draw() {
+		mat4.translate(this.trans, this.id, [0, this.y, 0]);
+		mat4.rotate(this.rot, this.id, this.ang, [0, 0, 1]);
+		mat4.mul(this.model, this.rot, this.id);
+		mat4.mul(this.model, this.trans, this.model);
+		gl.uniformMatrix4fv(this.uniModel, gl.FALSE, this.model);
+
+		// draw
+		gl.clearColor(0, 0, 0, 1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT);
+
+		gl.useProgram(this.prog.id);
+		gl.drawArrays(gl.LINES, 0, 2);
+	}
+}
+
 class Entity {
 	constructor() {
 		// data
@@ -168,44 +218,53 @@ document.addEventListener("DOMContentLoaded", function() {
 	}
 
 	// ship
-	ship = new Ship;
-	laser = new Laser;
+	// ship = new Ship;
+	// laser = new Laser;
 
 	document.addEventListener("keydown", function(e) {
 		switch (e.keyCode) {
 			case 37: // left
 				e.preventDefault();
 
-				ship.ang += 0.1;
+				// ship.ang += 0.1;
 
 				break;
 
 			case 39: // right
 				e.preventDefault();
 
-				ship.ang -= 0.1;
+				// ship.ang -= 0.1;
 
 				break;
 
 			case 40: // down
 				e.preventDefault();
 
-				ship.y -= 0.1;
+				// ship.y -= 0.1;
 
 				break;
 
 			case 38: // up
 				e.preventDefault();
 
-				ship.y += 0.1;
+				// ship.y += 0.1;
 
 				break;
 		}
 	});
 
+	mesh = new Mesh(
+		[
+			0.0, -0.5,
+			0.0, 0.5
+		]
+	);
+
 	function draw() {
 		// ship.draw();
-		laser.draw();
+		// laser.draw();
+
+		mesh.draw();
 
 		requestAnimationFrame(draw);
 	}
