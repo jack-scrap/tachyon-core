540b392ec4b998fc90db75dcd5f216abd778c874
jackhasakeyboard <jackhasakeyboard@gmail.com>
2020-07-25 09:45:21 -0600

    Drew lines in a closed loop again

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ class Mesh {
 		gl.uniformMatrix4fv(this.uniModel, gl.FALSE, this.model);
 
 		// draw
-		gl.drawArrays(gl.LINES, 0, this.vtc.length / 2);
+		gl.drawArrays(gl.LINE_LOOP, 0, this.vtc.length / 2);
 	}
 }
 
