960db8ac2cd731734804474fae86cc907fa9180b
jackhasakeyboard <jackhasakeyboard@gmail.com>
2020-07-25 09:04:55 -0600

    Squash

--- main.js
+++ main.js
@@ -33,6 +33,9 @@ class Mesh {
 	}
 
 	draw() {
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
+		gl.useProgram(this.prog.id);
+
 		mat4.translate(this.trans, this.id, [0, this.y, 0]);
 		mat4.rotate(this.rot, this.id, this.ang, [0, 0, 1]);
 		mat4.mul(this.model, this.rot, this.id);
@@ -43,166 +46,9 @@ class Mesh {
 		gl.clearColor(0, 0, 0, 1.0);
 		gl.clear(gl.COLOR_BUFFER_BIT);
 
-		gl.useProgram(this.prog.id);
 		gl.drawArrays(gl.LINES, 0, this.vtc.length / 2);
-
-		// console.log(this.vtc.length)
 	}
 }
-
-class Entity {
-	constructor() {
-		// data
-		this.vtc = [
-			0.0, 0.0,
-			0.0, 0.0
-		];
-
-		this.vbo = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vtc), gl.STATIC_DRAW);
-
-		this.prog = new Prog("shad");
-
-		// matrix
-		this.model = new Float32Array(16);
-
-		mat4.identity(this.model);
-
-		this.id = new Float32Array(16);
-		this.trans = new Float32Array(16);
-		this.rot = new Float32Array(16);
-
-		mat4.identity(this.id);
-
-		// attribute
-		this.attrPos = gl.getAttribLocation(this.prog.id, 'pos');
-		gl.vertexAttribPointer(this.attrPos, 2, gl.FLOAT, gl.FALSE, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
-		gl.enableVertexAttribArray(this.attrPos);
-
-		// uniform
-		this.uniModel = gl.getUniformLocation(this.prog.id, 'model');
-
-		this.y = 0;
-		this.ang = 0;
-	}
-
-	draw() {
-		mat4.translate(this.trans, this.id, [0, this.y, 0]);
-		mat4.rotate(this.rot, this.id, this.ang, [0, 0, 1]);
-		mat4.mul(this.model, this.rot, this.id);
-		mat4.mul(this.model, this.trans, this.model);
-		gl.uniformMatrix4fv(this.uniModel, gl.FALSE, this.model);
-
-		// draw
-		gl.clearColor(0, 0, 0, 1.0);
-		gl.clear(gl.COLOR_BUFFER_BIT);
-
-		gl.useProgram(this.prog.id);
-		gl.drawArrays(gl.LINES, 0, 2);
-	}
-};
-
-class Laser extends Entity {
-	constructor() {
-		super();
-
-		// data
-		this.vtc = [
-			0.0, -0.5,
-			0.0, 0.5
-		];
-
-		this.vbo = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vtc), gl.STATIC_DRAW);
-
-		this.prog = new Prog("shad");
-
-		// matrix
-		this.model = new Float32Array(16);
-
-		mat4.identity(this.model);
-
-		this.id = new Float32Array(16);
-		this.trans = new Float32Array(16);
-		this.rot = new Float32Array(16);
-
-		mat4.identity(this.id);
-
-		// attribute
-		this.attrPos = gl.getAttribLocation(this.prog.id, 'pos');
-		gl.vertexAttribPointer(this.attrPos, 2, gl.FLOAT, gl.FALSE, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
-		gl.enableVertexAttribArray(this.attrPos);
-
-		// uniform
-		this.uniModel = gl.getUniformLocation(this.prog.id, 'model');
-
-		this.y = 0;
-		this.ang = 0;
-	}
-};
-
-class Ship extends Entity {
-	constructor() {
-		super();
-
-		// data
-		this.vtc = [
-			0.0, 0.32,
-			-0.32, -0.5,
-
-			-0.32, -0.5,
-			0.32, -0.5,
-
-			0.32, -0.5,
-			0.0, 0.32
-		];
-
-		this.vbo = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vtc), gl.STATIC_DRAW);
-
-		this.prog = new Prog("shad");
-
-		// matrix
-		this.model = new Float32Array(16);
-
-		mat4.identity(this.model);
-
-		this.id = new Float32Array(16);
-		this.trans = new Float32Array(16);
-		this.rot = new Float32Array(16);
-
-		mat4.identity(this.id);
-
-		// attribute
-		this.attrPos = gl.getAttribLocation(this.prog.id, 'pos');
-		gl.vertexAttribPointer(this.attrPos, 2, gl.FLOAT, gl.FALSE, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
-		gl.enableVertexAttribArray(this.attrPos);
-
-		// uniform
-		this.uniModel = gl.getUniformLocation(this.prog.id, 'model');
-
-		this.y = 0;
-		this.ang = 0;
-	}
-
-	draw() {
-		mat4.translate(this.trans, this.id, [0, this.y, 0]);
-		mat4.rotate(this.rot, this.id, this.ang, [0, 0, 1]);
-		mat4.mul(this.model, this.rot, this.id);
-		mat4.mul(this.model, this.trans, this.model);
-		gl.uniformMatrix4fv(this.uniModel, gl.FALSE, this.model);
-
-		// draw
-		gl.clearColor(0, 0, 0, 1.0);
-		gl.clear(gl.COLOR_BUFFER_BIT);
-
-		gl.useProgram(this.prog.id);
-		gl.drawArrays(gl.LINES, 0, 2);
-	}
-};
 
 document.addEventListener("DOMContentLoaded", function() {
 	// initialize
@@ -218,10 +64,6 @@ document.addEventListener("DOMContentLoaded", function() {
 	if (!gl) {
 		alert('Initialization error: WebGL not supportd in browser');
 	}
-
-	// ship
-	// ship = new Ship;
-	// laser = new Laser;
 
 	document.addEventListener("keydown", function(e) {
 		switch (e.keyCode) {
@@ -257,8 +99,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	mesh = new Mesh(
 		[
-			0.0, -0.5,
-			0.0, 0.5
+			1.0, 0.5,
+			1.0, 1.5
 		]
 	);
 
@@ -272,9 +114,6 @@ document.addEventListener("DOMContentLoaded", function() {
 	);
 
 	function draw() {
-		// ship.draw();
-		// laser.draw();
-
 		mesh.draw();
 		// mesh1.draw();
 
